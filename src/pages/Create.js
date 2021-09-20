634132b8586dfb1837f3fec0c5c89c2fc972b41c
Irakli <koregm@gmail.com>
2021-09-20 20:30:35 +0400

    Typography

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import Typography from "@mui/material/Typography";
 
 export default function Create() {
   return (
     <div>
-      Create page
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom // add some margin on bottom
+      >
+        Create a new note
+      </Typography>
     </div>
-  )
+  );
 }
