173c3148271156ab294f74ae9804d3ce4a9d69d1
Irakli <koregm@gmail.com>
2021-09-21 17:17:33 +0400

    makeStyles Hook (Custom CSS)

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -2,12 +2,30 @@ import React from "react";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import { makeStyles } from "@mui/styles";
+
+// custom styles
+const useStyles = makeStyles({
+  btn: {
+    backgroundColor: "violet",
+    fontSize: 60,
+    "&:hover": {
+      backgroundColor: "blue",
+    },
+  },
+  title: {
+    textDecoration: "underline",
+    marginBottom: 20,
+  },
+});
 
 export default function Create() {
+  const classes = useStyles(); // call custom styles
   return (
     <Container>
       <Typography
+        className={classes.title}
         variant="h6"
         color="textSecondary"
         component="h2"
@@ -17,11 +35,12 @@ export default function Create() {
       </Typography>
 
       <Button
+        className={classes.btn}
         onClick={() => console.log("You clicked me")}
         type="submit"
         color="secondary"
         variant="contained"
-        endIcon={<KeyboardArrowRightIcon/>}
+        endIcon={<KeyboardArrowRightIcon />}
       >
         Submit
       </Button>
