37f8b34134846b728492842e31e1788b39cefd77
Irakli <koregm@gmail.com>
2021-09-22 13:50:31 +0400

    Grid System

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,23 +1,26 @@
-import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import { Container, Grid, Paper } from "@mui/material";
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function Notes() {
-  const [notes, setNotes] = useState([])
-
+  const [notes, setNotes] = useState([]);
 
   // get data from server and set it to notes state
   useEffect(() => {
-    fetch('http://localhost:8000/notes')
-    .then(res => res.json())
-    .then(data => setNotes(data))
-  
-  }, [])
+    fetch("http://localhost:8000/notes")
+      .then((res) => res.json())
+      .then((data) => setNotes(data));
+  }, []);
   return (
-    <div>
-      {notes.map(note => (
-        <p key={note.id}>{note.title}</p>
-      ))}
-    </div>
-  )
+    <Container>
+      <Grid container>
+        {notes.map((note) => (
+          <Grid item item xs={12} md={6} lg={4} key={note.id}>
+            <Paper>{note.title}</Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
 }
